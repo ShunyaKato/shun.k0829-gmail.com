688feee6ca53a207b2cf539af2e7dfa511ba3569
ShunyaKato <shun.k0829@gmail.com>
2020-05-20 15:41:21 +0900

    add loading to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Header :isFixed="isFixed" />
+    <Loading />
     <Slider :isFixed="isFixed" />
     <artistSection />
   </div>
@@ -10,6 +11,7 @@
 import "normalize.css";
 import { Component, Prop, Vue } from "vue-property-decorator";
 import Header from "@/components/Header";
+import Loading from "@/components/Loading";
 import Slider from "@/components/Slider";
 import artistSection from "@/components/artistSection";
 import { fetchApi } from "@/utils/index";
@@ -17,6 +19,7 @@ import { fetchApi } from "@/utils/index";
 @Component({
   components: {
     Header,
+    Loading,
     Slider,
     artistSection
   }
